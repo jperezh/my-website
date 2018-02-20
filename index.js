487a15ba9f5c0088ab6b4b2e44e679e638bb8ef6
Jaime Alejandro Perez <jperezh3@gmail.com>
2018-02-20 20:08:55 +0100

    Add old website files.

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/', (request, response) => {
 	response.sendFile(path.join(__dirname, '/index.html'));
 });
 
+app.use(express.static(path.join(__dirname, '/assets')));
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, '/404.html'))
+});
+
 app.listen(port, () => {
 	console.log("App has started listening on the port: " + port);
 });
