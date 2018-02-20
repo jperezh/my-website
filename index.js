ec58927b10f1c9b4b7702d7c61ad738bea2dede6
Jaime Alejandro Perez <jperezh3@gmail.com>
2018-02-20 19:57:27 +0100

    Initial commit, trying npm before moving the website.

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+
+const app = express();
+const port = 80;
+
+app.get('/', (request, response) => {
+	response.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(port, () => {
+	console.log("App has started listening on the port: " + port);
+});
