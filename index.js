74e5eb53af250601af8ccd4035c623e69545dd69
Jaime Alejandro Perez <jperezh3@gmail.com>
2018-02-20 20:01:47 +0100

    Change port number.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 80;
+const port = process.env.PORT || 5000
 
 app.get('/', (request, response) => {
 	response.sendFile(path.join(__dirname, '/index.html'));
